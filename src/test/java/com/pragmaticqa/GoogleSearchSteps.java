71bb24c57719a6b125285669bb9098bb68d531cc
Haroon Rasheed <haroonzone@gmail.com>
2012-10-30 00:00:42 +0000

    Remove cuke4duke plugin and update to cucumber-jvm.

--- src/test/java/com/pragmaticqa/GoogleSearchSteps.java
+++ src/test/java/com/pragmaticqa/GoogleSearchSteps.java
@@ -1,6 +1,8 @@
 package com.pragmaticqa;
 
-import cuke4duke.annotation.I18n;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
@@ -8,30 +10,24 @@ import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.containsString;
 
-/**
- * Created by IntelliJ IDEA.
- *
- * @author: Haroon Rasheed
- * Date: 18-Nov-2010
- */
 
-public class GoogleSearchFeature {
+public class GoogleSearchSteps {
 
     WebDriver driver;
 
-    @I18n.EN.Given("^The search is Hello World$")
+    @Given("^The search is Hello World$")
     public void theSearchIsHelloWorld() {
         driver = new HtmlUnitDriver();
         driver.get("http://www.google.co.uk");
         driver.findElement(By.xpath("//input[@name='q']")).sendKeys("Hello World");
     }
 
-    @I18n.EN.When("^The Search is performed$")
+    @When("^The Search is performed$")
     public void theSearchIsPerformed() {
         driver.findElement(By.xpath("//input[@name='btnG']")).click();
     }
 
-    @I18n.EN.Then("^The browser title should have Hello World$")
+    @Then("^The browser title should have Hello World$")
     public void theBrowserTitleShouldHaveHelloWorld() {
         assertThat ("Browser title:",driver.getTitle(), containsString("Hello World"));
         driver.quit();
