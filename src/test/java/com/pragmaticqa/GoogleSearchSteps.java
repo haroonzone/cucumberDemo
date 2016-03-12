d4371de79e0018a3ef21c51be3e98ef240c4c4c2
Haroon Rasheed <Haroon Rasheed>
2016-03-12 15:20:45 +0000

    Update the feature to work with firefox driver

--- src/test/java/com/pragmaticqa/GoogleSearchSteps.java
+++ src/test/java/com/pragmaticqa/GoogleSearchSteps.java
@@ -5,7 +5,10 @@ import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.containsString;
@@ -14,21 +17,28 @@ import static org.hamcrest.Matchers.containsString;
 public class GoogleSearchSteps {
 
     WebDriver driver;
+    WebElement searchInputField;
 
     @Given("^The search is Hello World$")
     public void theSearchIsHelloWorld() {
-        driver = new HtmlUnitDriver(true);
+        driver = new FirefoxDriver();
         driver.get("http://www.google.co.uk");
-        driver.findElement(By.xpath("//input[@name='q']")).sendKeys("Hello World");
+        searchInputField = driver.findElement(By.name("q"));
+        searchInputField.sendKeys("Hello World");
     }
 
     @When("^The Search is performed$")
     public void theSearchIsPerformed() {
-        driver.findElement(By.xpath("//input[@name='btnG']")).click();
+        searchInputField.submit();
     }
 
     @Then("^The browser title should have Hello World$")
     public void theBrowserTitleShouldHaveHelloWorld() {
+        (new WebDriverWait(driver, 5)).until(new ExpectedCondition<Boolean>() {
+            public Boolean apply(WebDriver d) {
+                return d.getTitle().toLowerCase().startsWith("hello world");
+            }
+        });
         assertThat ("Browser title:",driver.getTitle(), containsString("Hello World"));
         driver.quit();
     }
